0574a167156c33b75fa4e1c939a182530611cff3
rniemela77 <rvniemela@hotmail.com>
2024-11-18 20:47:00 -0700

    drag movement

--- game.js
+++ game.js
@@ -11,26 +11,83 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-let square;
-let moveUpButton;
-let moveDownButton;
-let moveDistance = 100;
-let targetY;
-let circle;
-let yellowCircleOutline;
-let healthBar;
-let healthBarBackground;
-let circleHealth;
+let square,
+  moveUpButton,
+  moveDownButton,
+  targetY,
+  circle,
+  yellowCircleOutline,
+  healthBar,
+  healthBarBackground;
+let joystick = null,
+  initialPointerY = 0,
+  velocityY = 0,
+  targetVelocityY = 0,
+  circleHealth = 100;
+const moveDistance = 100;
 
 function preload() {
   // Load any assets if necessary
 }
 
 function create() {
-  // Reset circle health
-  circleHealth = 100;
+  setupInputHandlers.call(this);
+  createGameObjects.call(this);
+  setupTimers.call(this);
+}
 
-  // Add a square in the center of the screen
+function update() {
+  updateJoystickVelocity();
+  applyJoystickVelocity();
+  moveCircleDownward();
+  updateUIPositions();
+  checkCollisions.call(this);
+}
+
+function setupInputHandlers() {
+  this.input.on("pointerdown", createJoystick, this);
+  this.input.on("pointermove", moveJoystick, this);
+  this.input.on("pointerup", removeJoystick, this);
+}
+
+function createGameObjects() {
+  createSquare.call(this);
+  createButtons.call(this);
+  createCircle.call(this);
+  createYellowCircleOutline.call(this);
+  createHealthBar.call(this);
+}
+
+function setupTimers() {
+  this.time.addEvent({
+    delay: 1000,
+    callback: fillYellowCircle,
+    callbackScope: this,
+    loop: true,
+  });
+}
+
+function createJoystick(pointer) {
+  initialPointerY = pointer.y;
+  joystick = this.add.circle(pointer.x, pointer.y, 30, 0x888888);
+}
+
+function moveJoystick(pointer) {
+  if (joystick) {
+    const deltaY = pointer.y - initialPointerY;
+    targetVelocityY = deltaY * 0.1; // Adjust the multiplier as needed
+  }
+}
+
+function removeJoystick() {
+  if (joystick) {
+    joystick.destroy();
+    joystick = null;
+    targetVelocityY = 0; // Reset target velocity when joystick is released
+  }
+}
+
+function createSquare() {
   square = this.add.rectangle(
     config.width / 2,
     config.height / 2,
@@ -38,8 +95,10 @@ function create() {
     50,
     0xff0000
   );
+  targetY = square.y;
+}
 
-  // Add up and down buttons
+function createButtons() {
   moveUpButton = this.add
     .text(config.width / 2 - 100, config.height - 150, "Up", {
       fontSize: "48px",
@@ -55,30 +114,22 @@ function create() {
     })
     .setInteractive()
     .on("pointerdown", () => moveSquare("down"));
+}
 
-  // Add a circle at the top of the screen, centered along the x-axis
+function createCircle() {
   circle = this.add.circle(config.width / 2, 0, 25, 0x0000ff);
+}
 
-  // Add a yellow circle outline 150px above the red square
+function createYellowCircleOutline() {
   yellowCircleOutline = this.add.circle(
     config.width / 2,
     config.height / 2 - 150,
     25
   );
   yellowCircleOutline.setStrokeStyle(2, 0xffff00);
+}
 
-  // Initialize targetY to the current position of the square
-  targetY = square.y;
-
-  // Add a timer event to fill the yellow circle every 1 second
-  this.time.addEvent({
-    delay: 1000,
-    callback: fillYellowCircle,
-    callbackScope: this,
-    loop: true,
-  });
-
-  // Add health bar background above the blue circle
+function createHealthBar() {
   healthBarBackground = this.add.rectangle(
     circle.x,
     circle.y - 35,
@@ -86,42 +137,42 @@ function create() {
     10,
     0x000000
   );
-
-  // Add health bar above the blue circle
   healthBar = this.add.rectangle(circle.x, circle.y - 35, 50, 10, 0xff0000);
 }
 
-function update() {
-  if (square.y !== targetY) {
-    square.y = Phaser.Math.Linear(square.y, targetY, 0.05);
-    if (Math.abs(square.y - targetY) < 1) {
-      square.y = targetY;
-      // Reset button opacity to full when the square stops moving
-      moveUpButton.setAlpha(1);
-      moveDownButton.setAlpha(1);
-    }
+function updateJoystickVelocity() {
+  const acceleration = 0.2; // Adjust the acceleration as needed
+  if (velocityY < targetVelocityY) {
+    velocityY = Math.min(velocityY + acceleration, targetVelocityY);
+  } else if (velocityY > targetVelocityY) {
+    velocityY = Math.max(velocityY - acceleration, targetVelocityY);
   }
+}
 
-  // Move the circle downward slowly
+function applyJoystickVelocity() {
+  square.y += velocityY;
+}
+
+function moveCircleDownward() {
   circle.y += 1;
+}
 
-  // Update the position of the yellow circle outline
+function updateUIPositions() {
   yellowCircleOutline.y = square.y - 150;
-
-  // Update the position of the health bar and its background
   healthBarBackground.x = circle.x;
   healthBarBackground.y = circle.y - 35;
   healthBar.x = circle.x;
   healthBar.y = circle.y - 35;
+}
 
-  // Check for collision between the blue circle and the red square
+function checkCollisions() {
   if (
     Phaser.Geom.Intersects.RectangleToRectangle(
       circle.getBounds(),
       square.getBounds()
     )
   ) {
-    this.scene.restart();
+    resetGame.call(this);
   }
 }
 
@@ -131,7 +182,6 @@ function moveSquare(direction) {
   } else if (direction === "down") {
     targetY = square.y + moveDistance;
   }
-  // Set button opacity to lower value when the square starts moving
   moveUpButton.setAlpha(0.5);
   moveDownButton.setAlpha(0.5);
 }
@@ -143,11 +193,8 @@ function fillYellowCircle() {
     25,
     0xffff00
   );
-  this.time.delayedCall(250, () => {
-    yellowCircle.destroy();
-  });
+  this.time.delayedCall(250, () => yellowCircle.destroy());
 
-  // Check for collision with the blue circle
   if (Phaser.Geom.Intersects.CircleToCircle(yellowCircle, circle)) {
     reduceCircleHealth.call(this);
   }
@@ -160,17 +207,23 @@ function reduceCircleHealth() {
   }
   healthBar.width = (circleHealth / 100) * 50;
 
-// Make it flash white
-const changeCircleColor = () => {
+  const changeCircleColor = () => {
     circle.setFillStyle(circle.fillColor === 0x0000ff ? 0xffffff : 0x0000ff);
-};
+  };
 
-for (let i = 50; i <= 200; i += 50) {
+  for (let i = 50; i <= 200; i += 50) {
     this.time.delayedCall(i, changeCircleColor);
-}
+  }
 
   if (circleHealth === 0) {
-    // Restart the scene if the health reaches 0
-    this.scene.restart();
+    resetGame.call(this);
   }
 }
+
+
+function resetGame() {
+    this.scene.restart();
+    circleHealth = 100;
+    healthBar.width = (circleHealth / 100) * 50;
+    circle.setFillStyle(0x0000ff);
+  }
