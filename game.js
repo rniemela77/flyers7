f139cd4ec7890e3d2d285eda3d1c6986ec85d75f
rniemela77 <rvniemela@hotmail.com>
2024-11-21 23:44:23 -0700

    turn game code into a module

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+import Phaser from "phaser";
+import { CONSTANTS } from "./src/constants";
+
 const config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
@@ -32,31 +35,6 @@ let joystick = null,
   targetVelocityY = 0,
   circleHealth1 = 100,
   circleHealth2 = 100;
-
-const CONSTANTS = {
-  gridSize: 800,
-  gridColor: 0x212121,
-  joystickRadius: 30,
-  joystickColor: 0x888888,
-  squareSize: 50,
-  squareColor: 0xff0000,
-  circleRadius: 25,
-  circleColor: 0x0000ff,
-  yellowCircleOutlineColor: 0xffff00,
-  healthBarWidth: 50,
-  healthBarHeight: 10,
-  healthBarBackgroundColor: 0x000000,
-  healthBarColor: 0xff0000,
-  acceleration: 0.1,
-  deceleration: 0.05,
-  maxSpeed: 5,
-  fillYellowCircleDelay: 1000,
-  fillYellowCircleDuration: 250,
-  reduceHealthAmount: 45,
-  resetHealth: 100,
-  lineAttackDamage: 20,
-  lineAttackDuration: 100, // duration in milliseconds
-};
 
 function preload() {
   // Load any assets if necessary
