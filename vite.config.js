dd946b82bf5f69058ff1c05d1927b7cc2646d03c
rniemela77 <rvniemela@hotmail.com>
2024-11-22 23:29:55 -0700

    style stuff

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    open: true,
-    host: true, // This will allow access from your local network
-    port: 3000, // You can change the port if needed
+    open: true, // open the browser when the server starts
+    host: true, // allow access on network
+    port: 3000, // change the port to 3000
   },
 });
